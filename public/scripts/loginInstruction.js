3ded14e4bab1c5664922ac5fe1c040ddbb11ee1c
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-29 22:34:00 -0600

    Pop up intro message added

---
+++ public/scripts/loginInstruction.js
@@ -0,0 +1,10 @@
+//Display the intructions Message
+swal("Wellcome to sloop.app", 'This is an enterprise application to manage employee feedback notes.\n' +
+'\nYou can add, edit, delete and manage new employees and the feedback provided by you or other leaders.\n'+
+'\nPlease login with:'+
+'\n   email: test@manager.com'+
+'\n   password: dev123\n\n' +
+'I hope you like it!');
+
+
+
