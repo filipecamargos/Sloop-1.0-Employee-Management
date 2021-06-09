097e136dcc55b0719c12aeaae221a083c05ace25
filipecamargos <filipe.atrabalho@gmail.com>
2021-06-08 23:04:17 -0600

    small fix

--- config/private.js
+++
@@ -1,8 +0,0 @@
-//Private Info to config
-
-exports.db = "mongodb+srv://cs341:VfP2xOJvOONEBDfK@cluster0-c8oj0.mongodb.net/Sloop?retryWrites=true&w=majority";
-exports.myUrl = "https://sloopapp.herokuapp.com";
-exports.mySecret = "Sloop";
-exports.provider = "gmail";
-exports.email = 'homeofficepost2020@gmail.com';
-exports.pass = 'homeoffice123';
